00a6532af6bb415ba09832aa940ae06980d08ed6
rdslade <rdslade>
2018-11-28 12:59:23 -0500

    Initial Commit

---
+++ C4CS/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  C4CS
+//
+//  Created by Ryan Slade on 11/27/18.
+//  Copyright © 2018 Ryan Slade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: Properties
+    @IBOutlet weak var timeUpdate: UIButton!
+    @IBOutlet weak var prompt: UITextView!
+    @IBOutlet weak var outputResponse: UITextView!
+    
+    func updateAllElementColor(color:UIColor){
+        self.view.backgroundColor = color;
+        outputResponse.backgroundColor = color;
+        prompt.backgroundColor = color;
+    }
+    
+    @objc func buttonClicked(_ sender: AnyObject?){
+        update();
+    }
+    
+    func update(){
+        let date = Date()
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: date)
+        let minutes = calendar.component(.minute, from: date)
+        let day = calendar.component(.weekday, from: date)
+        
+        var dayOfWeek = "";
+        switch(day){
+        case 1:
+            dayOfWeek = "Sunday";
+        case 2:
+            dayOfWeek = "Monday";
+            break;
+        case 3:
+            dayOfWeek = "Tuesday";
+            break;
+        case 4:
+            dayOfWeek = "Wednesday";
+            break;
+        case 5:
+            dayOfWeek = "Thursday";
+            break;
+        case 6:
+            dayOfWeek = "Friday";
+            break;
+        case 7:
+            dayOfWeek = "Saturday";
+            break;
+        default:
+            dayOfWeek = "Error";
+        }
+        
+        timeUpdate.setTitle("Updated: \(dayOfWeek) @ \(hour):\(minutes)", for: .normal);
+        
+        if(day == 6 && hour >= 10 && hour <= 12){
+            outputResponse.text = "YES! :)"
+            updateAllElementColor(color: UIColor.green);
+        }
+        else{
+            outputResponse.text = "No :("
+            updateAllElementColor(color: UIColor.red);
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        update()
+        timeUpdate.addTarget(self, action: #selector(buttonClicked(_:)), for: .touchUpInside)
+    }
+
+
+}
+
---
+++ C4CSTests/C4CSTests.swift
@@ -0,0 +1,34 @@
+//
+//  C4CSTests.swift
+//  C4CSTests
+//
+//  Created by Ryan Slade on 11/27/18.
+//  Copyright © 2018 Ryan Slade. All rights reserved.
+//
+
+import XCTest
+@testable import C4CS
+
+class C4CSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
